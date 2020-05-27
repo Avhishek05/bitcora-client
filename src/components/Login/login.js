5e2e34ea486a1b3151c133d6be605d7d6c3912f6
Abhishek Yadav <avhishekydv@gmail.com>
2020-05-27 21:22:07 +0530

    login/sign up page added

--- src/components/Login/login.js
+++ src/components/Login/login.js
@@ -1,23 +1,179 @@
-/**
- * Created by Abhi on 03-05-2020.
- */
 import React from "react";
 import {connect} from "react-redux";
 import {searchText} from "../../store/actions/actions";
-import {Button} from "antd";
+import {Button, Form, Input, Checkbox, Col, Row} from "antd";
 import "./styles.scss";
+
+const layout = {
+    labelCol: {
+        span: 8,
+    },
+    wrapperCol: {
+        span: 16,
+    },
+};
+const tailLayout = {
+    wrapperCol: {
+        offset: 8,
+        span: 16,
+    },
+};
+
+
 class LoginUser extends React.Component {
 
-    onSubmit = () => {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showLoginForm: true,
+            showSignupForm: false,
+        }
+    }
+
+    onFinish = (values) => {
+        alert(JSON.stringify(values));
         this.props.history.push("/home");
+
+    };
+
+    onFinishsignUp = (values) => {
+        alert(JSON.stringify(values));
+        this.props.history.push("/home");
+
+    };
+
+    onFinishFailed = errorInfo => {
+        console.log('Failed:', errorInfo);
+    };
+
+    onShow = (field) => {
+        if(field==='login'){
+        this.setState({showLoginForm: true,showSignupForm: false})
+        }
+        else
+            this.setState({showLoginForm: false,showSignupForm: true});
+
     };
 
     render() {
         return (
             <div>
+                <Row>
+                    <Col offset={6} span={6}>
+                        <Button onClick={()=>this.onShow('login')}>
+                            Login
+                        </Button>
+                    </Col>
+                    <Col offset={6} span={6}>
+                        <Button onClick={()=>this.onShow('signup')}>
+                            Sign Up
+                        </Button>
+                    </Col>
+                </Row>
+                {this.state.showLoginForm && !this.state.showSignupForm &&
                 <div className="login-warp login-component">
-                    <Button type="primary" title="Go" onClick={this.onSubmit}>GO</Button>
-                </div>
+                    <Form
+                        {...layout}
+                        name="basic"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={this.onFinish}
+                        onFinishFailed={this.onFinishFailed}
+                    >
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your email!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+
+                        <Form.Item {...tailLayout}>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+
+                </div>}
+                
+                {!this.state.showLoginForm && this.state.showSignupForm &&
+                <div className="login-warp login-component">
+                    <Form
+                        {...layout}
+                        name="basic"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={this.onFinishsignUp}
+                        onFinishFailed={this.onFinishFailed}
+                    >
+                        <Form.Item
+                            label="Email"
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your email!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+                        <Form.Item
+                            label="Name"
+                            name="name"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your name!',
+                                },
+                            ]}
+                        >
+                            <Input />
+                        </Form.Item>
+
+                        <Form.Item
+                            label="Password"
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Please input your password!',
+                                },
+                            ]}
+                        >
+                            <Input.Password />
+                        </Form.Item>
+                        <Form.Item {...tailLayout}>
+                            <Button type="primary" htmlType="submit">
+                                Submit
+                            </Button>
+                        </Form.Item>
+                    </Form>
+
+                </div>}
+                
+                
             </div>
         );
     }
